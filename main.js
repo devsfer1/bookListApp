c45e77f2cb8b5535562014104fcf68428e8f72c8
Fernando <fernando.chaves16@hotmail.com>
2021-02-13 19:12:57 -0300

    inputs defined in JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const isbnInput = document.getElementById('isbnInput');
+
+const form = document.getElementById('form');
+
+
 
 
 
