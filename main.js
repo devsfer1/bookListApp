f07b460118757954fa68334c245938672c9ddebd
Fernando <fernando.chaves16@hotmail.com>
2021-02-14 08:11:39 -0300

    table added

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+// get DOM values
 const titleInput = document.getElementById('titleInput');
 const authorInput = document.getElementById('authorInput');
-const isbnInput = document.getElementById('isbnInput');
+const genreInput = document.getElementById('genreInput');
 
 const form = document.getElementById('form');
+
+// 
+
+
 
 
 
