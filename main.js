4798d0ce55a6901e06e52e55d87a975641a19b2f
Fernando <fernando.chaves16@hotmail.com>
2021-02-15 03:37:17 -0300

    Book class created

--- main.js
+++ main.js
@@ -2,10 +2,19 @@
 const titleInput = document.getElementById('titleInput');
 const authorInput = document.getElementById('authorInput');
 const genreInput = document.getElementById('genreInput');
-
 const form = document.getElementById('form');
 
-// 
+// book class
+class Book {
+  constructor(title, author, genre) {
+    this.title = title,
+    this.author = author,
+    this.genre = genre
+  }
+} 
+
+
+
 
 
 
